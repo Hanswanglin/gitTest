8fc466adccbaae85cc1a90518ea9de3af037951a
HansWanglin <hans15280248245@gmail.com>
2018-05-28 12:14:10 +0800

    Also make some different changes to 1.c on this branch

--- 1.c
+++ 1.c
@@ -2,6 +2,6 @@
 
 int main(){
 	printf("this is the file in SE_lab11\n");
-	printf("make some changes to 1.c on this branch\n");
+	printf("Also make some different changes to 1.c on this branch\n");
 	return 0;
 }
